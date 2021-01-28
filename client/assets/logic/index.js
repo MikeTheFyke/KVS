2c877067bc2d9b73a24c7d230eeb56bd7a13b8b4
MikeTheFyke <mikefyke@hotmail.com>
2021-01-27 21:31:46 -0500

    Adding body color fade

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -1,2 +1,3 @@
 TweenLite.to("#header-container", 0, { y: "30vw", scaleY: 0 });
 TweenLite.to("#header-container", 1, { y: "30vw", scaleY: 1, delay: 1 });
+TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
