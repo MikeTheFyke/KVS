6129b847454d203db4651fb2018f9839396699e5
MikeTheFyke <mikefyke@hotmail.com>
2021-01-27 22:08:31 -0500

    adding timeline gsap styles to header

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -1,3 +1,5 @@
-TweenLite.to("#header-container", 0, { y: "30vw", scaleY: 0 });
-TweenLite.to("#header-container", 1, { y: "30vw", scaleY: 1, delay: 1 });
+// var tl = new TimelineMax();
+
+TweenLite.to("#header-container", 0, { y: "20vw", scaleY: 0 });
+TweenLite.to("#header-container", 1, { scaleY: 1, transformOrigin: "center center", delay: 1 });
 TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
