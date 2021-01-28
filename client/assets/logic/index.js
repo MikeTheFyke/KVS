99172f982e118b6555996a91e3f43cd992178e45
MikeTheFyke <mikefyke@hotmail.com>
2021-01-27 21:30:47 -0500

    The header is gsap expanding

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -1,1 +1,2 @@
-TweenLite.to("#header-container", 0, { y: "30vw" });
+TweenLite.to("#header-container", 0, { y: "30vw", scaleY: 0 });
+TweenLite.to("#header-container", 1, { y: "30vw", scaleY: 1, delay: 1 });
