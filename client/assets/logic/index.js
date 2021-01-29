50e29eef587b55aed84605a313fb98ec6d55f995
MikeTheFyke <mikefyke@hotmail.com>
2021-01-28 21:11:02 -0500

    Adding gsap to title bar animation

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -3,3 +3,6 @@
 TweenLite.to("#header-container", 0, { y: "20vw", scaleY: 0 });
 TweenLite.to("#header-container", 1, { scaleY: 1, transformOrigin: "center center", delay: 1 });
 TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
+
+TweenLite.to("#top-bar", 0, { y: "-120px", opacity: 0 })
+TweenLite.to("#top-bar", 0.5, { y: 0, opacity: 1, delay: 1.5 })
