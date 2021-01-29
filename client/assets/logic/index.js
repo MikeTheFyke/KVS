29f98e0460c6615e13960334e9dc012da68cd4c3
MikeTheFyke <mikefyke@hotmail.com>
2021-01-28 21:13:33 -0500

    added easing to title-bar animation

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -5,4 +5,4 @@ TweenLite.to("#header-container", 1, { scaleY: 1, transformOrigin: "center cente
 TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
 
 TweenLite.to("#top-bar", 0, { y: "-120px", opacity: 0 })
-TweenLite.to("#top-bar", 0.5, { y: 0, opacity: 1, delay: 1.5 })
+TweenLite.to("#top-bar", 0.5, { y: 0, opacity: 1, delay: 1.5, ease: "elastic.out(1, 0.5)" })
