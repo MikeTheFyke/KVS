8aca21421adcace2541cef42bd927682b2ff2a96
MikeTheFyke <mikefyke@hotmail.com>
2021-01-28 21:26:25 -0500

    Adding title text styles

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -7,5 +7,7 @@ TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
 TweenLite.to("#top-bar", 0, { y: "-120px", opacity: 0 })
 TweenLite.to("#bottom-bar", 0, { y: "-240px", opacity: 0 })
 
-TweenLite.to("#top-bar", 0.5, { y: 0, opacity: 1, delay: 1.5, ease: "elastic.out(1, 0.5)" })
+TweenLite.to("#top-bar", 0.5, { y: "50px", opacity: 1, delay: 1.5 })
+TweenLite.to("#top-bar", 0.25, { y: 0, delay: 1.75, ease: "elastic.out(1, 0.5)" })
+
 TweenLite.to("#bottom-bar", 0.5, { y: 0, opacity: 1, delay: 1.5, ease: "elastic.out(1, 0.5)" })
