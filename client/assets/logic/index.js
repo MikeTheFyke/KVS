65dde10c6220b4870f97c3f4af49fa5a940ede55
MikeTheFyke <mikefyke@hotmail.com>
2021-01-29 20:51:35 -0500

    Added kenny title gsap animation

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -6,8 +6,10 @@ TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
 
 TweenLite.to("#top-bar", 0, { y: "-120px", opacity: 0 })
 TweenLite.to("#bottom-bar", 0, { y: "-240px", opacity: 0 })
+TweenLite.to("#kenny-title", 0, { y: "-120px", opacity: 0, rotation: "-45" })
 
 TweenLite.to("#top-bar", 0.5, { y: "50px", opacity: 1, delay: 1.5 })
-TweenLite.to("#top-bar", 0.25, { y: 0, delay: 1.75, ease: "elastic.out(1, 0.5)" })
-
 TweenLite.to("#bottom-bar", 0.5, { y: 0, opacity: 1, delay: 1.5, ease: "elastic.out(1, 0.5)" })
+
+TweenLite.to("#top-bar", 0.25, { y: 0, delay: 1.75, ease: "elastic.out(1, 0.5)" })
+TweenLite.to("#kenny-title", 0.75, { y: 0, opacity: 1, delay: 1.75, rotation: 0, ease: "elastic.out(1, 0.5)" })
