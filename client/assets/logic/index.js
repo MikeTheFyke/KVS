7a0bddd24a323b79221e693c5de4822c03f26376
MikeTheFyke <mikefyke@hotmail.com>
2021-01-29 21:46:08 -0500

    added yellow star rotational gsap animation

--- client/assets/logic/index.js
+++ client/assets/logic/index.js
@@ -2,6 +2,7 @@
 
 TweenLite.to("#header-container", 0, { y: "20vw", scaleY: 0 });
 TweenLite.to("#vs-container", 0, { y: "-120px", opacity: 0 });
+TweenLite.to("#yellow-star", 0, { y: "-120px", opacity: 0 });
 TweenLite.to("#header-container", 1, { scaleY: 1, transformOrigin: "center center", delay: 1 });
 TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
 
@@ -10,6 +11,8 @@ TweenLite.to("#bottom-bar", 0, { y: "-240px", opacity: 0 })
 TweenLite.to("#kenny-title", 0, { y: "-120px", opacity: 0, rotation: "-45" })
 TweenLite.to("#spenny-title", 0, { y: "-120px", opacity: 0, rotation: "45" })
 
+TweenLite.to("#yellow-star", 0.5, { y: 0, opacity: 1, delay: 1.5 });
+TweenLite.to("#yellow-star", 2.5, { delay: 1.5, rotation: 359, repeat: -1, ease: "Linear.easeInOut"  });
 TweenLite.to("#top-bar", 0.5, { y: "50px", opacity: 1, delay: 1.5 })
 TweenLite.to("#bottom-bar", 0.5, { y: 0, opacity: 1, delay: 1.5, ease: "elastic.out(1, 0.5)" })
 
