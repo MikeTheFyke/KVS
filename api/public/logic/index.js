5cf10f41f172cb999ff3ed61b120881e1cfe993a
MikeTheFyke <mikefyke@hotmail.com>
2021-02-01 20:47:02 -0500

    Added client data to api

---
+++ api/public/logic/index.js
@@ -0,0 +1,26 @@
+// var tl = new TimelineMax();
+
+TweenLite.to("#header-container", 0, { y: "20vw", scaleY: 0 });
+TweenLite.to("#vs-container", 0, { y: "-120px", opacity: 0 });
+TweenLite.to("#yellow-star", 0, { y: "-120px", opacity: 0 });
+TweenLite.to(".black-star", 0, { y: "-120px", opacity: 0 });
+TweenLite.to("#header-container", 1, { scaleY: 1, transformOrigin: "center center", delay: 1 });
+TweenLite.to("body", 1, { backgroundColor: "white", delay: 1 });
+
+TweenLite.to("#top-bar", 0, { y: "-120px", opacity: 0 })
+TweenLite.to("#bottom-bar", 0, { y: "-240px", opacity: 0 })
+TweenLite.to("#kenny-title", 0, { y: "-120px", opacity: 0, rotation: "-45" })
+TweenLite.to("#spenny-title", 0, { y: "-120px", opacity: 0, rotation: "45" })
+
+TweenLite.to(".black-star", 0.5, { y: 0, opacity: 1, delay: 1.5 });
+TweenLite.to("#yellow-star", 0.5, { y: 0, opacity: 1, delay: 1.5 });
+TweenLite.to("#yellow-star", 3.5, { delay: 1.5, rotation: 359, repeat: -1, ease: "Linear.easeInOut"  });
+TweenLite.to("#top-bar", 0.5, { y: "50px", opacity: 1, delay: 1.5 })
+TweenLite.to("#bottom-bar", 0.5, { y: 0, opacity: 1, delay: 1.5, ease: "elastic.out(1, 0.5)" })
+
+TweenLite.to("#top-bar", 0.25, { y: 0, delay: 1.75, ease: "elastic.out(1, 0.5)" })
+TweenLite.to("#kenny-title", 0.75, { y: 0, opacity: 1, delay: 1.75, rotation: 0, ease: "elastic.out(1, 0.5)" })
+TweenLite.to("#spenny-title", 0.75, { y: 0, opacity: 1, delay: 1.75, rotation: 0, ease: "elastic.out(1, 0.5)" })
+TweenLite.to("#vs-container", 0.5, { y: 0, opacity: 1, ease: "elastic.out(1, 0.5)", delay: 2.25 });
+
+TweenLite.to("#header-container", 1, { y:0 , delay: 3})
---
+++ api/views/home.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/index.css" type="text/css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <title>K VS S</title>
+</head>
+<body>
+
+<div id="header-container">
+    <div id="black-star-left" class="black-star"></div>
+    <div id="black-star-right" class="black-star"></div>
+    <div id="yellow-star"></div>
+    <div id="top-bar" class="header-bars"></div>
+    <div id="bottom-bar" class="header-bars"></div>
+    <p id="kenny-title" class="header-text">KENNY</p>
+            <div id="vs-container">
+            <div id="vs-top" class="vs-bars"></div>
+                <p id="vs-text">VS</p>
+            <div id="vs-bottom" class="vs-bars"></div>
+            </div>
+    <p id="spenny-title" class="header-text">SPENNY</p>
+    <div id="kenny-head" class="title-heads"></div>
+    <div id="spenny-head" class="title-heads"></div>
+</div>
+
+</body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+    <script src="../assets/logic/index.js"></script>
+</html>
