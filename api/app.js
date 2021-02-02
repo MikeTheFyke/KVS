348dfc14a13f9ed608008ba4115e96fca9dc2193
MikeTheFyke <mikefyke@hotmail.com>
2021-02-01 20:55:57 -0500

    Starting to route home in express

--- api/app.js
+++ api/app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', (req, res) => {
-  res.render('/index')
+  res.render("home")
 })
 
 app.listen(port, () => {
